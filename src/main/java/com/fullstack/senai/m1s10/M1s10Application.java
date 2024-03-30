3be6e22f61a356dbefc8f6524c192ad6b98e2e6d
Marcos Vinicios <mvdias1268@gmail.com>
2024-03-30 12:31:32 -0300

    adicionado pastas

---
+++ src/main/java/com/fullstack/senai/m1s10/M1s10Application.java
@@ -0,0 +1,13 @@
+package com.fullstack.senai.m1s10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1s10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M1s10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstack/senai/m1s10/M1s10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.senai.m1s10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1s10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
