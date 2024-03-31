41396ebff34027b2de4b648b79b86fc06d5ab130
marcosvnDias <150302006+marcosvnDias@users.noreply.github.com>
2024-03-31 11:20:03 -0300

    Update MaterialController.java
    aparentemente apaguei sem querem o nome do endpoint do MaterialController

--- src/main/java/com/fullstack/senai/m1s10/controllers/MaterialController.java
+++ src/main/java/com/fullstack/senai/m1s10/controllers/MaterialController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(" ")
+@RequestMapping("materiais")
 public class MaterialController {
     private final MaterialServiceImpl service;
 
